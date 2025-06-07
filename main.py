38c085341ff588cd7b2f766bea0339c758ec18f1
kelsier <evan4556376@yahoo.com.tw>
2025-06-07 16:53:01 +0800

    f

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ from sqlalchemy import create_engine, Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from passlib.context import CryptContext
-
+# form openai import AzureOpenAI
+# from dotenv import load_dotenv
 # Create FastAPI app
 app = FastAPI(title="Login API")
 
